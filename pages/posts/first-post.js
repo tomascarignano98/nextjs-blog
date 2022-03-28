ae546732ea96e313b582012f58f215a2b022b663
tomascaringnao98 <tomascaringnao98@gmail.com>
2022-03-28 11:19:24 -0300

    Assets.

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function FirstPost() {
   return (
     <>
+      <img src="/images/profile.jpg" alt="Your Name" />
       <h1>First Post</h1>
       <h2>
         <Link href="/">
